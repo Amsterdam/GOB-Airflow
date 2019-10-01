9d64f54acc8ef068fa4752a6abbc9d4c46df98c1
Nick Barykine <nick.barykine@gmail.com>
2019-10-01 11:29:07 +0200

    [GOB-1028] Improve import_dag so we can easily add new pipelines

--- src/dags/import_dag.py
+++ src/dags/import_dag.py
@@ -11,11 +11,12 @@ from sensors.consume import ConsumeMessageSensor
 default_args = {
     'owner': 'airflow',
     'depends_on_past': False,
-    'start_date': datetime(2019, 9, 18),
+    'start_date': datetime(2019, 10, 1),
     'email': ['nick.barykine@gmail.com'],
     'email_on_failure': False,
     'email_on_retry': False,
 }
+
 
 dag = DAG('import_dag', default_args=default_args)
 
@@ -23,124 +24,106 @@ start = DummyOperator(
     task_id='start',
     dag=dag)
 
-import_nap_peilmerken_start = PublishMessageOperator(
-    task_id="import_nap_peilmerken_start",
-    action="import",
-    message_id="import_nap_peilmerken",
-    catalogue="nap",
-    collection="peilmerken",
-    dag=dag)
-
-import_nap_peilmerken_end = ConsumeMessageSensor(
-    task_id='import_nap_peilmerken_end',
-    action="import",
-    message_id="import_nap_peilmerken",
-    mode='reschedule',
-    poke_interval=30,
-    timeout=60,
-    dag=dag)
-
-compare_nap_peilmerken_start = PublishMessageOperator(
-    task_id="compare_nap_peilmerken_start",
-    action="compare",
-    message_id="compare_nap_peilmerken",
-    source_action="import",
-    source_message_id="import_nap_peilmerken",
-    dag=dag)
-
-compare_nap_peilmerken_end = ConsumeMessageSensor(
-    task_id='compare_nap_peilmerken_end',
-    action="compare",
-    message_id="compare_nap_peilmerken",
-    mode='reschedule',
-    poke_interval=30,
-    timeout=60,
-    dag=dag)
-
-fullupdate_nap_peilmerken_start = PublishMessageOperator(
-    task_id="fullupdate_nap_peilmerken_start",
-    action="fullupdate",
-    message_id="fullupdate_nap_peilmerken",
-    source_action="compare",
-    source_message_id="compare_nap_peilmerken",
-    dag=dag)
-
-fullupdate_nap_peilmerken_end = ConsumeMessageSensor(
-    task_id='fullupdate_nap_peilmerken_end',
-    action="fullupdate",
-    message_id="fullupdate_nap_peilmerken",
-    ack=True,
-    mode='reschedule',
-    poke_interval=30,
-    timeout=60,
-    dag=dag)
-
-# # gebieden:stadsdelen
-import_gebieden_stadsdelen_start = PublishMessageOperator(
-    task_id="import_gebieden_stadsdelen_start",
-    action="import",
-    message_id="import_gebieden_stadsdelen",
-    catalogue="gebieden",
-    collection="stadsdelen",
-    application="DGDialog",
-    dag=dag)
-
-import_gebieden_stadsdelen_end = ConsumeMessageSensor(
-    task_id='import_gebieden_stadsdelen_end',
-    action="import",
-    message_id="import_gebieden_stadsdelen",
-    mode='reschedule',
-    poke_interval=30,
-    timeout=60,
-    dag=dag)
-
-compare_gebieden_stadsdelen_start = PublishMessageOperator(
-    task_id="compare_gebieden_stadsdelen_start",
-    action="compare",
-    message_id="compare_gebieden_stadsdelen",
-    source_action="import",
-    source_message_id="import_gebieden_stadsdelen",
-    dag=dag)
-
-compare_gebieden_stadsdelen_end = ConsumeMessageSensor(
-    task_id='compare_gebieden_stadsdelen_end',
-    action="compare",
-    message_id="compare_gebieden_stadsdelen",
-    mode='reschedule',
-    poke_interval=30,
-    timeout=60,
-    dag=dag)
-
-fullupdate_gebieden_stadsdelen_start = PublishMessageOperator(
-    task_id="fullupdate_gebieden_stadsdelen_start",
-    action="fullupdate",
-    message_id="fullupdate_gebieden_stadsdelen",
-    source_action="compare",
-    source_message_id="compare_gebieden_stadsdelen",
-    dag=dag)
-
-fullupdate_gebieden_stadsdelen_end = ConsumeMessageSensor(
-    task_id='fullupdate_gebieden_stadsdelen_end',
-    action="fullupdate",
-    message_id="fullupdate_gebieden_stadsdelen",
-    ack=True,
-    mode='reschedule',
-    poke_interval=30,
-    timeout=60,
-    dag=dag)
-
-
 end = DummyOperator(
     task_id='end',
     dag=dag)
 
 
-start >> import_nap_peilmerken_start >> import_nap_peilmerken_end \
-      >> compare_nap_peilmerken_start >> compare_nap_peilmerken_end \
-      >> fullupdate_nap_peilmerken_start >> fullupdate_nap_peilmerken_end \
-      >> end
+def make_message_id(action, catalogue, collection):
+    return "%s_%s_%s" % (action, catalogue, collection)
 
-start >> import_gebieden_stadsdelen_start >> import_gebieden_stadsdelen_end \
-      >> compare_gebieden_stadsdelen_start >> compare_gebieden_stadsdelen_end \
-      >> fullupdate_gebieden_stadsdelen_start >> fullupdate_gebieden_stadsdelen_end \
-      >> end
+def make_task_id(message_id, type):
+    return "%s_%s" % (message_id, type)
+
+def add_pipeline_to_dag(catalogue, collection, application):
+    mode="reschedule"
+    poke_interval=30
+    timeout=60
+
+    action_import = "import"
+    message_id_import = make_message_id(action_import, catalogue, collection)
+
+    action_compare = "compare"
+    message_id_compare = make_message_id(action_compare, catalogue, collection)
+
+    action_fullupdate = "fullupdate"
+    message_id_fullupdate = make_message_id(action_fullupdate, catalogue, collection)
+
+    import_start = PublishMessageOperator(
+        task_id=make_task_id(message_id_import, "start"),
+        action=action_import,
+        message_id=message_id_import,
+        catalogue=catalogue,
+        collection=collection,
+        application=application,
+        dag=dag)
+
+    import_end = ConsumeMessageSensor(
+        task_id=make_task_id(message_id_import, "end"),
+        action=action_import,
+        message_id=message_id_import,
+        mode=mode,
+        poke_interval=poke_interval,
+        timeout=timeout,
+        dag=dag)
+
+    compare_start = PublishMessageOperator(
+        task_id=make_task_id(message_id_compare, "start"),
+        action=action_compare,
+        message_id=message_id_compare,
+        source_action=action_import,
+        source_message_id=message_id_import,
+        dag=dag)
+
+    compare_end = ConsumeMessageSensor(
+        task_id=make_task_id(message_id_compare, "end"),
+        action=action_compare,
+        message_id=message_id_compare,
+        mode=mode,
+        poke_interval=poke_interval,
+        timeout=timeout,
+        dag=dag)
+
+    fullupdate_start = PublishMessageOperator(
+        task_id=make_task_id(message_id_fullupdate, "start"),
+        action=action_fullupdate,
+        message_id=message_id_fullupdate,
+        source_action=action_compare,
+        source_message_id=message_id_compare,
+        dag=dag)
+
+    fullupdate_end = ConsumeMessageSensor(
+        task_id=make_task_id(message_id_fullupdate, "end"),
+        action=action_fullupdate,
+        message_id=message_id_fullupdate,
+        ack=True,
+        mode=mode,
+        poke_interval=poke_interval,
+        timeout=timeout,
+        dag=dag)
+
+    start >> import_start >> import_end \
+          >> compare_start >> compare_end \
+          >> fullupdate_start >> fullupdate_end \
+          >> end
+
+
+pipelines = [
+    {
+        "catalogue": "nap",
+        "collection": "peilmerken",
+        "application": None,
+    },
+    {
+        "catalogue": "gebieden",
+        "collection": "stadsdelen",
+        "application": "DGDialog",
+    },
+]
+
+for pipeline in pipelines:
+    add_pipeline_to_dag(
+        pipeline["catalogue"],
+        pipeline["collection"],
+        pipeline["application"]
+    )
